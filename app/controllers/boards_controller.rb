78c3d245d944280a56f48a7456fced93bf066a59
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-11-16 11:06:05 +0900

    createアクションを追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,22 @@
 class BoardsController < ApplicationController
-  before_action :authenticate_user!, only: [ :new ]
+  before_action :authenticate_user!, only: [ :new, :create ]
 
   def new
     @board = current_user.boards.build
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
   end
 
 end
