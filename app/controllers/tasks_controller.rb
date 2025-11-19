1ac59be3957c1c092b54eb22a7b2af33d60019f0
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-11-19 17:01:37 +0900

    newアクションを編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,13 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!, only: [ :new ]
+  before_action :set_board, only: [ :new ]
 
   def new
-    @task = build_task
+    @task = @board.tasks.build(user: current_user)
   end
 
-  def current_board
-    Board.find(params[:board_id])
-  end
-
-  def build_task
-    current_board.tasks.build(user_id: current_user[:id])
+  def set_board
+    @board = Board.find(params[:board_id])
   end
 
 end
